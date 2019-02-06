12038de83561a211e861abf8e579707400ed3ffb
Negin Firouzian <negfir@gmail.com>
2019-02-06 10:42:22 +0330

    Add files via upload

---
+++ Project2/src/main.cpp
@@ -0,0 +1,724 @@
+#include "mbed.h"
+#include "time.h"
+
+
+PwmOut buzzer(PTD2);
+ 
+int main(){
+    wait(0.6);
+   
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+   
+   
+    //C3 
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+    
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.25);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3 
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+    // end first time
+   
+     //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //Bb2
+    buzzer.period(1.0/116.54);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+     
+    //B2
+    buzzer.period(1.0/123.47);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;
+    wait(0.25);
+    buzzer=0.0;
+    wait(0.1);
+    
+    wait(0.25);
+    
+    //end second time
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.25);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+    //end third time
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.25);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+    //end Fourth time
+
+//second line
+
+ //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //Bb2
+    buzzer.period(1.0/116.54);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+     
+    //B2
+    buzzer.period(1.0/123.47);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;
+    wait(0.25);
+    buzzer=0.0;
+    wait(0.1);
+    
+    wait(0.25);
+    
+    //end first time
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+    
+    //A3
+    buzzer.period(1.0/220.0);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //A3
+    buzzer.period(1.0/220.0);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //G3
+    buzzer.period(1.0/196.00);
+    buzzer =0.5;
+    wait(0.25);
+    buzzer=0.0;
+    wait(0.1);
+
+    //G3
+    buzzer.period(1.0/196.00);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //G3
+    buzzer.period(1.0/196.00);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+//end second time
+    //G3
+    buzzer.period(1.0/196.00);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C4
+    buzzer.period(1.0/261.63);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C4
+    buzzer.period(1.0/261.63);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.25);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+//end third time
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+//end forth time
+
+//F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //F3   
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //B2   
+    buzzer.period(1.0/123.47);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+//third line
+
+    
+    //F2
+    buzzer.period(1.0/87.31);
+    buzzer =0.5;
+    wait(0.25);
+    buzzer=0.0;
+    wait(0.1);
+    wait(0.25);
+   
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+//end first time
+
+ //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+ //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+ //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+ //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);  
+
+//end second time
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+  
+    //Bb2
+    buzzer.period(1.0/116.54);
+    buzzer =0.5;
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F2
+    buzzer.period(1.0/87.31);
+    buzzer =0.5;
+    wait(0.25);
+    buzzer=0.0;
+    wait(0.1);
+
+wait(0.25);
+
+//end third time
+
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.25);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+//end forth time
+
+//F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+  //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.25);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //F3
+    buzzer.period(1.0/174.61);
+    buzzer =0.5;    
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+
+    //C3
+    buzzer.period(1.0/130.81);
+    buzzer =0.5;   
+    wait(0.125);
+    buzzer=0.0;
+    wait(0.1);
+//forth line
+
+
+   
+   
+}
